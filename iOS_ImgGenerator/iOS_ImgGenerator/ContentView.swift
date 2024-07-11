2c57e355418f47793b9dd889836c4e066d734199
Hashwim <ss991117@naver.com>
2024-07-11 18:34:17 +0900

    [Add] add model and StableDiffusion

---
+++ iOS_ImgGenerator/iOS_ImgGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_ImgGenerator
+//
+//  Created by 서수영 on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_ImgGenerator/iOS_ImgGenerator/iOS_ImgGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ImgGeneratorApp.swift
+//  iOS_ImgGenerator
+//
+//  Created by 서수영 on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ImgGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
