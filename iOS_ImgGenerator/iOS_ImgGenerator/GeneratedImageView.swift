ef8b10ca45ae6907a0ddc0930ba7be9205aafda3
Hashwim <ss991117@naver.com>
2024-07-16 00:42:05 +0900

    [Feat] 생성 이미지 뷰 구현 및 화면 이동 애니메이션 구현

---
+++ iOS_ImgGenerator/iOS_ImgGenerator/GeneratedImageView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneratedImageView.swift
+//  iOS_ImgGenerator
+//
+//  Created by 서수영 on 7/16/24.
+//
+
+import SwiftUI
+
+struct GeneratedImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneratedImageView()
+}
--- iOS_ImgGenerator/iOS_ImgGenerator/TextToImageView.swift
+++ iOS_ImgGenerator/iOS_ImgGenerator/TextToImageView.swift
@@ -18,31 +18,80 @@ lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer
                                            seed: 1_000_000,
                                            stepCount: 20,
                                            imageCount: 1, disableSafety: false)
+
+    @State var isGenerating: Bool = false
+
+    @Namespace var topID
+    @Namespace var imgTopID
+
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Text to image").font(.title3).bold().padding(6)
-                Text("guide text")
-                    .foregroundColor(.secondary)
-                    .font(.caption)
-                    .padding(.bottom)
+        ScrollViewReader { proxy in
+            ScrollView {
+                VStack {
+                    Text("Text to image").font(.title3).bold().padding(6)
+                        .id(topID)
+                    Text("guide text")
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                        .padding(.bottom)
 
-                PromptView(parameter: $generationParameter)
-                    .disabled(imageGenerator.generationState != .idle)
+                    PromptView(parameter: $generationParameter)
+                        .disabled(imageGenerator.generationState != .idle)
 
-                if imageGenerator.generationState == .idle {
-                    Button(action: generate) {
-                        Text("Generate").font(.title)
-                    }.buttonStyle(.borderedProminent)
-                } else {
-                    ProgressView()
-                }
+                    if imageGenerator.generationState == .idle {
+                        Button(action: {
+                            generate()
+                            isGenerating = true
+                            withAnimation {
+                                proxy.scrollTo(imgTopID, anchor: .top)
+                            }
+                        }) {
+                            Text("Generate").font(.title)
+                        }.buttonStyle(.borderedProminent)
+                    } else {
+                        Text("Generating..").font(.title)
+                    }
 
-                if let generatedImages = imageGenerator.generatedImages {
-                    ForEach(generatedImages.images) {
-                        Image(uiImage: $0.uiImage)
-                            .resizable()
-                            .scaledToFit()
+                    Spacer().id(imgTopID)
+
+                    if isGenerating {
+                        if let generatedImages = imageGenerator.generatedImages {
+                            ForEach(generatedImages.images) {
+                                Image(uiImage: $0.uiImage)
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            Button(action: {
+                                print("")
+                                withAnimation {
+                                    proxy.scrollTo(topID, anchor: .top)
+                                }
+                            }, label: {
+                                Text("Save")
+                                    .font(.title)
+                            })
+                            .buttonStyle(.borderedProminent)
+                        } else {
+                            VStack {
+                                Image("gen_image")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .symbolEffect(.variableColor.cumulative, options: .repeating.speed(1.5))
+                                    .id(imgTopID)
+
+                                Button(action: {
+                                    print("")
+                                    isGenerating = false
+                                    withAnimation {
+                                        proxy.scrollTo(topID, anchor: .top)
+                                    }
+                                }, label: {
+                                    Text("Cancel")
+                                        .font(.title)
+                                })
+                                .buttonStyle(.borderedProminent)
+                            }.frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+                        }
                     }
                 }
             }
