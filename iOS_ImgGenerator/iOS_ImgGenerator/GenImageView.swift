4e21965d36bc3c066a2b9fb0461bd087195e69ee
Hashwim <ss991117@naver.com>
2024-07-22 17:19:34 +0900

    [Refactor] GenImageView getGeneratedImages메서드를 통한 중복 코드 제거

--- iOS_ImgGenerator/iOS_ImgGenerator/GenImageView.swift
+++ iOS_ImgGenerator/iOS_ImgGenerator/GenImageView.swift
@@ -2,100 +2,66 @@
 import SwiftUI
 
 struct GenImageView: View {
+
+    @EnvironmentObject var imageGenerator: ImageGenerator
+
     @Binding var isGenerating: Bool
     @Binding var isSaved: Bool
     var isT2I: Bool
-    @EnvironmentObject var imageGenerator: ImageGenerator
 
     var body: some View {
         VStack {
             if isGenerating {
-                if isT2I {
-                    HStack {
-                        Text("Generated Image")
-                            .font(.headline)
+                HStack {
+                    Text("Generated Image")
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                    Spacer()
+                }
+
+                if let generatedImages = getGeneratedImages() {
+                    ForEach(generatedImages.images) { image in
+                        Image(uiImage: image.uiImage)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    }
+                    Button(action: {
+                        let imageSaver = ImageSaver()
+                        imageSaver.writeToPhotoAlbum(image: generatedImages.images.first!.uiImage)
+                        isSaved = true
+                    }, label: {
+                        Text(!isSaved ? "Save" : "Complete!")
                             .foregroundStyle(Color.white)
-                        Spacer()
-                    }
-
-                    if let generatedImages = imageGenerator.t2iGeneratedImages {
-                        ForEach(generatedImages.images) { image in
-                            Image(uiImage: image.uiImage)
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
-                        }
-                        Button(action: {
-                            let imageSaver = ImageSaver()
-                            imageSaver.writeToPhotoAlbum(image: generatedImages.images.first!.uiImage)
-                            isSaved = true
-                        }, label: {
-                            Text(!isSaved ? "Save" : "Complete!")
-                                .foregroundStyle(Color.white)
-                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        })
-                        .buttonStyle(.borderedProminent)
-                        .tint(MySpecialColors.accentDeepRed)
-                        .disabled(isSaved)
-                    } else {
-                        CircularProgressView(progress: (Double(imageGenerator.steps) / 28.1))
-
-                        Button(action: {
-                            print("")
-                            imageGenerator.isCancelled = true
-                            isGenerating = false
-                        }, label: {
-                            Text("Cancel")
-                                .font(.title)
-                        })
-                        .buttonStyle(.borderedProminent)
-                        .opacity(0.8)
-                        .tint(MySpecialColors.accentDeepRed)
-                    }
+                            .font(.title)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .tint(MySpecialColors.accentDeepRed)
+                    .disabled(isSaved)
                 } else {
-                    HStack {
-                        Text("Generated Image")
-                            .font(.headline)
-                            .foregroundStyle(Color.white)
-                        Spacer()
-                    }
+                    CircularProgressView(progress: (Double(imageGenerator.steps) / 28.1))
 
-                    if let generatedImages = imageGenerator.i2iGeneratedImages {
-                        ForEach(generatedImages.images) { image in
-                            Image(uiImage: image.uiImage)
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
-                        }
-                        Button(action: {
-                            let imageSaver = ImageSaver()
-                            imageSaver.writeToPhotoAlbum(image: generatedImages.images.first!.uiImage)
-                            isSaved = true
-                        }, label: {
-                            Text(!isSaved ? "Save" : "Complete!")
-                                .foregroundStyle(Color.white)
-                                .font(.title)
-                        })
-                        .buttonStyle(.borderedProminent)
-                        .tint(MySpecialColors.accentDeepRed)
-                        .disabled(isSaved)
-                    } else {
-                        CircularProgressView(progress: (Double(imageGenerator.steps) / 28.1))
-
-                        Button(action: {
-                            print("")
-                            imageGenerator.isCancelled = true
-                            isGenerating = false
-                        }, label: {
-                            Text("Cancel")
-                                .font(.title)
-                        })
-                        .buttonStyle(.borderedProminent)
-                        .opacity(0.8)
-                        .tint(MySpecialColors.accentDeepRed)
-                    }
+                    Button(action: {
+                        print("")
+                        imageGenerator.isCancelled = true
+                        isGenerating = false
+                    }, label: {
+                        Text("Cancel")
+                            .font(.title)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .opacity(0.8)
+                    .tint(MySpecialColors.accentDeepRed)
                 }
             }
         }.padding()
+    }
+
+    private func getGeneratedImages() -> ImageGenerator.GeneratedImages? {
+        if isT2I {
+            return imageGenerator.t2iGeneratedImages
+        } else {
+            return imageGenerator.i2iGeneratedImages
+        }
     }
 }
